34e771b55436c49c6e62f3368a5cf6eaef24df5c
malong5 <malong5@gome.com.cn>
2020-04-03 00:17:13 +0800

    支持Androidx 版本V1.0.1

--- mvp/src/main/java/com/ml512/base/BaseActivity.java
+++ mvp/src/main/java/com/ml512/base/BaseActivity.java
@@ -2,8 +2,8 @@ package com.ml512.base;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
